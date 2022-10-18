ec77949d94f84b5d239a473569ab67bfbac24025
youdaehoon <107843412+youdaehoon@users.noreply.github.com>
2022-10-18 23:04:05 +0900

    백준 15일

--- 정렬/좌표 압축/app.js
+++ 정렬/좌표 압축/app.js
@@ -1,17 +1,35 @@
+// //** 메모리 초과 */
+// let fs=require("fs")
+// let file=process.platform=="linux"?0:__dirname+"/input.txt"
+// let input=fs.readFileSync(file,{encoding:"utf-8"}).trim().split("\n")
+// input.shift()
+// input=input[0].split(" ").map(Number)
+// let setInput=[...new Set(input)]
+// let ans=""
+// let cnt
+// for(let idx in input){
+//     cnt=0
+//     setInput.map((v)=>{
+        
+//         if(v <input[idx])++cnt
+//     })
+//     ans+=cnt+" "
+// }
+// console.log(ans.trim())
+// //** */
+
 let fs=require("fs")
 let file=process.platform=="linux"?0:__dirname+"/input.txt"
 let input=fs.readFileSync(file,{encoding:"utf-8"}).trim().split("\n")
 input.shift()
 input=input[0].split(" ").map(Number)
-let setInput=[...new Set(input)]
-let ans=""
-let cnt
+
+const setArr=[...new Set(input)].sort((a,b)=>a-b)
+let obj={}
+setArr.map((v,i)=>obj[v]=i)
+let ans=[]
 for(let idx in input){
-    cnt=0
-    setInput.map((v)=>{
-        
-        if(v <input[idx])++cnt
-    })
-    ans+=cnt+" "
+ans.push(obj[input[idx]])
 }
-console.log(ans.trim())
+console.log(ans.join(" "))
+
